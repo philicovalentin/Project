1b7288e7f96c57c5fe8915fba2656be9995b2410
walidbenhammoud <walidosbh@gmail.com>
2015-11-25 11:42:31 +0100

    Collection
    A collection was created to insert the personal data

--- simple-todos.js
+++ simple-todos.js
@@ -1,3 +1,5 @@
+
+Personalinfo = new Mongo.Collection("personalinfo");
 Languages = new Mongo.Collection("languages");
 Degrees = new Mongo.Collection("degrees");
 Experiences = new Mongo.Collection("experiences");
@@ -17,10 +19,100 @@ if (Meteor.isClient) {
 
   });
 
-
+Template.addPersonalInfo.events({
+  'submit form': function(event){
+    console.log("Form submitted");
+    console.log(event.type);
+    event.preventDefault();
+    var finame=event.target.text.value;
+    var faname=event.target.text.value;
+    var street=event.target.text.value;
+    var zip=event.target.text.value;
+    var city=event.target.text.value;
+    var nationality=event.target.text.value;
+    /*var Source tax=event.target.text.value*/
+    var dnumber=event.target.text.value;
+    var mnumber=event.target.text.value;
+    var birth=event.target.date.value;
+    var ahv=event.target.text.value;
+    var sdate=event.target.date.value;
+    var mdate=event.target.date.value;
+    var sfaname=event.target.text.value;
+    var sfiname=event.target.text.value;
+    var ch1name=event.target.text.value;
+    var ch1bdate=event.target.date.value;
+    var ch2name=event.target.text.value;
+    var ch2bdate=event.target.date.value;
+    var ch3name=event.target.text.value;
+    var ch3bdate=event.target.date.value;
+    var em1name=event.target.text.value;
+    var relation1=event.target.text.value;
+    var phone1=event.target.text.value;
+    var em2name=event.target.text.value;
+    var relation2=event.target.text.value;
+    var phone2=event.target.text.value;
+    var bankname=event.target.text.value;
+    var bankplace=event.target.text.value;
+    var bankzip=event.target.text.value;
+    var iban=event.target.text.value;
+    var accnbr=event.target.text.value;
+    var accname=event.target.text.value;
+    var eurbankname=event.target.text.value;
+    var eurbankplace=event.target.text.value;
+    var eurbankzip=event.target.text.value;
+    var euriban=event.target.text.value;
+    var euraccnbr=event.target.text.value;
+    var euraccname=event.target.text.value;
+    
+    Personalinfo.insert({
+     faname: fname,
+     street: street,
+     zip: zip,
+     city: city,
+     nationality: nationality,
+    /* Source tax=event.target.text.value*/
+     dnumber: dnumber,
+     mnumber: mnumber,
+     birth: birth,
+     ahv: ahv,
+     sdate: sdate,
+     mdate: mdate,
+     sfaname: sfaname,
+     sfiname: sfiname,
+     ch1name: ch1name,
+     ch1bdate: ch1bdate,
+     ch2name: ch2name,
+     ch2bdate: ch2bdate,
+     ch3name: ch3name,
+     ch3bdate: ch3bdate,
+     em1name: em1name,
+     relation1: relation1,
+     phone1: phone1,
+     em2name: em2name,
+     relation2: relation2,
+     phone2: phone2,
+     bankname: bankname,
+     bankplace: bankplace,
+     bankzip: bankzip,
+     iban: iban,
+     accnbr: accnbr,
+     accname: accname,
+     eurbankname: eurbankname,
+     eurbankplace: eurbankplace,
+     eurbankzip: eurbankzip,
+     euriban: euriban,
+     euraccnbr: euraccnbr,
+     euraccname: euraccname,
+     createdAt: new Date(),
+     owner: Meteor.userId(),
+     /*username: Meteor.user().username*/  // username of logged in user
+    });
+    event.target.text.value= "";
+  },
+})
   
 Template.body.events({
-    "submit .addlang": function (event) {
+    "submit .new-language": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
       // Get value from form element
@@ -28,21 +120,14 @@ Template.body.events({
       var skill= event.target.skill.value;
       // Insert a task into the collection
             Languages.insert({
-
         text: text,
-
         skill: skill,
-
         createdAt: new Date(),            // current time
-
         owner: Meteor.userId(),           // _id of logged in user
-
         /*username: Meteor.user().username*/  // username of logged in user
-
       });
       // Clear form
       event.target.text.value = "";
-
     },
 
     /*"change .hide-completed input": function (event) {
